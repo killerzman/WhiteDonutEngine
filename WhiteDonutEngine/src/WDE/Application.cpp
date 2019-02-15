4f44892868a883cae8e9204c373684b203fe78bc
Trăistaru Vlad-Viorel <31930649+killerzman@users.noreply.github.com>
2019-02-15 15:41:42 +0200

    Added GLFW events dispatching

--- WhiteDonutEngine/src/WDE/Application.cpp
+++ WhiteDonutEngine/src/WDE/Application.cpp
@@ -1,21 +1,31 @@
 #include "wdepch.h"
 #include "Application.h"
 
-#include "WDE/Events/ApplicationEvent.h"
 #include "WDE/Log.h"
 
 #include "GLFW/glfw3.h"
 
 namespace WDE {
 
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		WDE_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -25,6 +35,12 @@ namespace WDE {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
